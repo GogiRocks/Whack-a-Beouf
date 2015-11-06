46a5d471ac551c9047580a39e9fad4415403bc06
GogiRocks <aph@mchsi.com>
2015-11-05 18:13:11 -0600

    Created SpriteKit Project

---
+++ Whack-a-Beouf/Whack-a-Beouf/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Whack-a-Beouf
+//
+//  Created by Alexander Harper on 11/5/15.
+//  Copyright (c) 2015 Gogi Region. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    //location where all sprites need to be rendered
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
